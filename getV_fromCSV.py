ce6f0dc2037ed73d243ec8c411feb9f2d9733e86
YiyeZhang <ichiyoz@gmail.com>
2020-01-27 15:03:41 -0500

    getV

--- getV_fromCSV.py
+++ getV_fromCSV.py
@@ -10,22 +10,12 @@ import os
 from collections import OrderedDict
 
 path =os.path.expanduser('~/Documents/Data/NYPData/HF/')
-# path = os.path.expanduser('~/Documents/Data/OMOP/')
 
 
 class Structure:
-    def filterData(self, data):
-
-        eligible=[]
-        for pid in data3:   
-            if len(data3[pid]['appt'])>1:
-                for date in data3[pid]['appt']:
-                    age=int(date[0:4])-data3[pid]['age']
-                    if age>0 and age<90:
-                        eligible.append(pid)
-                    break
-        print(len(eligible))        
+    def filterData(self, data):     
         
+        #find a list of all unique diagnoses
         diaglist = list()
         for pid in data:
             if len(data[pid]['appt']) != 0:
@@ -40,11 +30,13 @@ class Structure:
                             for i in data[pid]['appt'][date]['withinappt'][time]['diag']:
                                 for key, value in i.items():
                                     diaglist.append(value)
-                                    
+
+        #diagnosis frequency                           
         c = Counter(diaglist)
         c_most_common = [item[0] for item in c.most_common(50)]
         print ('c_most_common', c_most_common)
         
+        #find a list of all unique orders
         orderlist = []
         for pid in data:
             if len(data[pid]['appt']) != 0:
@@ -56,11 +48,12 @@ class Structure:
                         for time in sorted(iter(data[pid]['appt'][date]['withinappt'])):
                             for p in data[pid]['appt'][date]['withinappt'][time]['proc']:
                                 orderlist.append(p)
-
+        #order frequency    
         cp = Counter(orderlist)
         cp_most_common = [item[0] for item in cp.most_common(50)]
         print ('cp_most_common', cp_most_common)
 
+        #find a list of all unique drugs
         druglist = []
         for pid in data:
             if len(data[pid]['appt']) != 0:
@@ -73,11 +66,12 @@ class Structure:
                         for time in sorted(iter(data[pid]['appt'][date]['withinappt'])):
                             for p in data[pid]['appt'][date]['withinappt'][time]['drug']:
                                 druglist.append(p['name'])
+        #drug frequency
         cd = Counter(druglist)
         cd_most_common = [item[0] for item in cd.most_common(50)]
         print ('cd_most_common', cd_most_common)
 
-
+        #re-label encounter type
         for pid in data:
             for date in sorted(iter(data[pid]['appt'])):
                 # if data[pid]['appt'][date]['type'] == 'Emergency Room and Inpatient Visit':
@@ -97,17 +91,18 @@ class Structure:
         readmitdata = {}  # full data of patients who had 30-day readmission
         eligible=[]
         
+        #age eligibility
         for pid in data:   
           if len(data[pid]['appt'])>1:
             for date in data[pid]['appt']:
                 age=int(date[0:4])-data[pid]['age']
-                if age>0 and age<90:
+                if age>17 and age<90:
                     eligible.append(pid)
                 break
         print(len(eligible))
 
+        #filter by age eligibility
         data2 = deepcopy(data)
-
         for pid in data:
             if pid in eligible:
                 for date in sorted(data[pid]['appt']):
@@ -130,18 +125,10 @@ class Structure:
                         if str(data[pid]['appt'][date]['drug'][icd]) in cd_most_common and str(
                                 data[pid]['appt'][date]['drug'][icd]) not in data2[pid]['appt'][date]['drugclass']:
                             data2[pid]['appt'][date]['drugclass'].append(str(data[pid]['appt'][date]['drug'][icd]))
-
             else:
                 del data2[pid]
 
-        data3 = deepcopy(data2)
-        for pid in data2:
-            if pid in ('9001105983400270','9001107845700270'):
-                pass
-            else:
-                del data3[pid]
-
-                    
+        
         pickle_out = open(path + 'HF_data_filtered.pickle', 'wb')
         pickle.dump(data2, pickle_out)
         pickle_out.close()
@@ -152,18 +139,9 @@ class Structure:
         with open(JSON_FILE, 'w') as outfile:
             json.dump(data2, outfile, indent=2, sort_keys=True, separators=(',', ': '))
 
-        # pickle_out = open(path + 'HF_readmit.pickle', 'wb')
-        # pickle.dump(readmitlist, pickle_out)
-        # pickle_out.close()
-
-        # pickle_out = open(path + 'HF_readmit_data.pickle', 'wb')
-        # pickle.dump(readmitdata, pickle_out)
-        # pickle_out.close()
-        # print(len(data2))
         return data2
 
     def getNode(self, data):
-
         diaglist = list()
         for pid in data:
             if len(data[pid]['appt']) != 0:
@@ -323,116 +301,33 @@ class Structure:
 
         return data, Vdesc
 
-    def getSeq(self, data, Vdesc):
-        VT = dict()
-        tempDT = dict()
-
-        for pid in data:
-            t = Structure()
-            VT[pid] = list()
-            # tempDT[pid] = list()
-            # VT[pid].append('start')
-            # tempDT[pid].append('start')
-            for date in sorted(data[pid]['appt']):
-                if 'withinappt' not in data[pid]['appt'][date] or len(data[pid]['appt'][date]['withinappt']) == 0:
-                    # tempDT[pid].append(data[pid]['appt'][date]['actualdate'])
-                    VT[pid].append(Vdesc[str(data[pid]['appt'][date]['type']) + str(
-                        data[pid]['appt'][date]['diag']) + str(data[pid]['appt'][date]['proc']) + str(
-                        data[pid]['appt'][date]['drugclass'])])
-                elif len(data[pid]['appt'][date]['withinappt']) > 0:
-                    for time in sorted(iter(data[pid]['appt'][date]['withinappt'])):
-                        # tempDT[pid].append(data[pid]['appt'][date]['withinappt'][time]['actualtime'])
-                        VT[pid].append(Vdesc[str(data[pid]['appt'][date]['type']) + str(
-                            data[pid]['appt'][date]['withinappt'][time]['diag']) + str(
-                            data[pid]['appt'][date]['withinappt'][time]['proc']) + str(
-                            data[pid]['appt'][date]['withinappt'][time]['drugclass'])])
-
-        print('getSeq1', datetime.now())
-
-        # for pid in VT:
-        #     if len(VT[pid])>1:
-        #         print (pid, data[pid]['location'], VT[pid])
-        print('getSeq2', datetime.now())
-        # VT=self.findRepeats(VT)
-        print (len(VT))
-        pickle_out = open(path + 'HF_VT.pickle', 'wb')
-        pickle.dump(VT, pickle_out)
-        pickle_out.close()
-        return VT, tempDT
-
-    def findRepeats(self, VT):
-        seen = {}
-
-        uniq = {}
-        # IF GET ACTUAL REPEATS
-        for pid in VT:
-            seen[pid] = set()
-            uniq[pid] = {}
-            for x in range(len(VT[pid])):
-                if VT[pid][x] not in seen[pid]:
-                    uniq[pid][VT[pid][x]] = 0
-                    uniq[pid][VT[pid][x]] = 1 + uniq[pid][VT[pid][x]]
-                    seen[pid].add(VT[pid][x])
-                else:
-                    uniq[pid][VT[pid][x]] = uniq[pid][VT[pid][x]] + 1
-                    VT[pid][x] = str(VT[pid][x]) + '_' + str(uniq[pid][VT[pid][x]])
-
-        # #IF ONLY LABEL 
-        # for pid in VT:
-        #     seen[pid]=set()
-        #     uniq[pid]={}
-        #     for x in range(len(VT[pid])):
-        #         if VT[pid][x] not in seen[pid]:
-        #             uniq[pid][VT[pid][x]]=0
-        #             uniq[pid][VT[pid][x]]=1+uniq[pid][VT[pid][x]]
-        #             seen[pid].add(VT[pid][x])
-        #         else:
-        #             uniq[pid][VT[pid][x]]=uniq[pid][VT[pid][x]]
-        #             VT[pid][x]=str(VT[pid][x])+'_'+str(uniq[pid][VT[pid][x]])
-        return VT
-
-
 def main():
     d = Structure()
 
     fh = open(path + "EDDC_HF_ouput.json", 'r')
     data = json.load(fh)
-    
-    # SEE JUPITER printDiscofile
-
-    # pickle_out = open(path+'HF_data.pickle','rb')
-    # data = pickle.load(pickle_out)
-    # pickle_out.close()
     print ('data loaded')
     print ('num patient',len(data))
     
-    # data2 = d.filterData(data)
-    # print ('data filtered')
+    data2 = d.filterData(data)
+    print ('data filtered')
     
-    pickle_out = open(path + 'data_18_89_EDDC_2012_2018_filtered.pickle', 'rb')
-    data2 = pickle.load(pickle_out)
-    pickle_out.close()
+    # pickle_out = open(path + 'data_18_89_EDDC_2012_2018_filtered.pickle', 'rb')
+    # data2 = pickle.load(pickle_out)
+    # pickle_out.close()
     # fh = open(path + "HF_data_filtered_OS.json", 'r')
     # data2 = json.load(fh)
 
+    #make into sequences
     nodedesc = d.getNode(data2)
     pickle_out = open(path + 'HF_node.pickle', 'rb')
     nodedesc = pickle.load(pickle_out)
     pickle_out.close()
-
     data2, Vdesc = d.getV(nodedesc, data2)
     pickle_out = open(path + 'HF_V.pickle', 'rb')
     Vdesc = pickle.load(pickle_out)
     pickle_out.close()
-    VT, tempDT = d.getSeq(data2, Vdesc)  ##LINE 226!!!!!
-
     
-    # pickle_out = open(path+'_V_data.pickle','rb')
-    # data2 = pickle.load(pickle_out)
-    # pickle_out.close()
-    # print ('V',Vdesc)
-
-    #data2=d.filterV(data2,nodedesc)
     
 
 if __name__ == '__main__':
