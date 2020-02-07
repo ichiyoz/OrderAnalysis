811dc10741679de4d8184c63bf7aa5ce0723656c
ichiyoz <ichiyoz@gmail.com>
2020-02-07 10:13:44 -0500

    R

--- LCS_clustering.R
+++ LCS_clustering.R
@@ -2,11 +2,11 @@ library(cluster)
 library(TraMineR)
 library(sparcl)
 #use the printout from getV_fromCSV.py (line 332)
-data=read.csv("/Users/yiyezhang/Dropbox/Grant-and-IRB/AHRQ R03/Analysis/EDDC_order_18_89_seq.csv",header=FALSE)
-
+data=read.csv("/Users/yiz2014/Dropbox/Grant-and-IRB/AHRQ R03/Analysis/EDDC_18_89_med_HF_seq_task.csv",header=FALSE)
+N=len(data)
 #data2=subset(data,V2==3)  #subset clusters
 #N is len of data
-data_seq=seqdef(data[,5:N],left="DEL",right = "DEL",gaps="DEL")  #look at first 4 events in the sequence
+data_seq=seqdef(data[,3:30],left="DEL",right = "DEL",gaps="DEL")  #look at first 4 events in the sequence
 
 data_lcs <- seqdist(data_seq, method = "LCS")
 clusterward_lcs <- agnes(data_lcs, diss = TRUE, method = "ward")
